7dc95d0d3a11d924fa45220aaa8d3439ea5b3fab
Victor Romero <romeroa.victor@yahoo.com>
2020-08-19 15:45:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:known_for, :languages][0]
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -88,7 +88,7 @@ def adding_matz
       }
  		  
  		}
-programmer_hash
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def changing_alan
